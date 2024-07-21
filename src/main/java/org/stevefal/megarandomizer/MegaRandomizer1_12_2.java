c3513abf36e74b1fa09039e4c43771555fb2dcfb
stevefali <139910217+stevefali@users.noreply.github.com>
2024-07-21 02:25:57 -0700

    Call RandomDrops.shuffleItems() when server is ready.

--- src/main/java/org/stevefal/megarandomizer/MegaRandomizer1_12_2.java
+++ src/main/java/org/stevefal/megarandomizer/MegaRandomizer1_12_2.java
@@ -1,5 +1,6 @@
 package org.stevefal.megarandomizer;
 
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -7,10 +8,10 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
 import org.apache.logging.log4j.Logger;
 import org.stevefal.megarandomizer.gamerules.MegaGameRules;
+import org.stevefal.megarandomizer.megadrops.RandomDrops;
 
 @Mod(useMetadata = true, modid = MegaRandomizer1_12_2.MODID, name = MegaRandomizer1_12_2.NAME, version = MegaRandomizer1_12_2.VERSION)
-public class MegaRandomizer1_12_2
-{
+public class MegaRandomizer1_12_2 {
     public static final String MODID = "megarandomizer1_12_2";
     public static final String NAME = "Mega Randomizer 1.12.2";
     public static final String VERSION = "1.0-1.12.2";
@@ -18,19 +19,19 @@ public class MegaRandomizer1_12_2
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
 
     }
 
     @EventHandler
     public void onServerReady(FMLServerStartedEvent event) {
         MegaGameRules.register();
+
+        RandomDrops.shuffleItems(FMLCommonHandler.instance().getMinecraftServerInstance().getEntityWorld().getSeed());
     }
 }
